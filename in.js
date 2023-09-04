59f656575354701be2b1ce854b2ee42faf33545f
Vanica <88245937+VanicaK@users.noreply.github.com>
2023-09-04 18:47:16 +0100

    Add files via upload

---
+++ in.js
@@ -0,0 +1,17 @@
+let words=[
+    {
+        "inputs":5,
+        "category":"games",
+        "word":"chess"
+    },
+    {
+        "inputs":6,
+        "category":"countries",
+        "word":"france"
+    },
+    {
+        "inputs":6,
+        "category":"colours",
+        "word":"yellow"
+    }
+]
